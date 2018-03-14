fd84ac1e844c3bb459507fc7418b11535a75f2c8
Ivan <ivanyn9@gmail.com>
2018-03-14 17:16:26 +0100

    Prototipo de la descarga de datos
    #10

--- web/views.py
+++ web/views.py
@@ -9,3 +9,18 @@ def index():
     Página de inicio
     """
     return render_template('index.html')
+
+
+@app.route('/consulta')
+def renta_burgos():
+    """
+    Ejemplo de consulta
+    """
+    fuente = 'aeat_renta'
+    filtro = {
+        'Municipio': 'Burgos',
+        'RENTA BRUTA MEDIA': {'$lte': 26}
+    }
+    cursor = mongo.db[fuente].find(filtro)
+    df = pd.DataFrame.from_records(cursor, exclude=['_id'])
+    return df.to_html()
