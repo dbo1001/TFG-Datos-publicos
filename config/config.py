291aa43ea65fd8b2f35857d179f29a1782227f1c
Ivan <ivanyn9@gmail.com>
2018-03-19 15:43:18 +0100

    Añadida una clave secreta a la configuración

--- config/config.py
+++ config/config.py
@@ -3,6 +3,8 @@ class Config(object):
     Configuración de la aplicación
     """
     DEBUG = True
+    SECRET_KEY = 'r^Ic3@LoDbhnQ3kQr5t1vP6XZJdZ!!sc'
+    # MongoDB
     MONGO_HOST = 'localhost'
     MONGO_PORT = 27017
     MONGO_DBNAME = 'datos'
