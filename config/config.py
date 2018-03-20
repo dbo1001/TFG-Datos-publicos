170e924324f36602f26448d601478f848eb298c9
Ivan <ivanyn9@gmail.com>
2018-03-20 22:17:00 +0100

    Actualizadas variables de entorno de nanobox
    #12

--- config/config.py
+++ config/config.py
@@ -8,8 +8,8 @@ class Config(object):
     DEBUG = True
     SECRET_KEY = 'r^Ic3@LoDbhnQ3kQr5t1vP6XZJdZ!!sc'
     # MongoDB
-    MONGO_HOST = os.environ.get('MONGO_HOST', 'localhost')
-    MONGO_PORT = int(os.environ.get('MONGO_PORT', 27017))
-    MONGO_DBNAME = os.environ.get('MONGO_DBNAME', 'datos')
-    MONGO_USERNAME = os.environ.get('MONGO_USERNAME', None)
-    MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD', None)
+    MONGO_HOST = os.environ.get('DATA_DB_HOST', 'localhost')
+    MONGO_PORT = int(os.environ.get('DATA_DB_PORT', 27017))
+    MONGO_DBNAME = os.environ.get('DATA_DB_NAME', 'datos')
+    MONGO_USERNAME = os.environ.get('DATA_DB_USER', None)
+    MONGO_PASSWORD = os.environ.get('DATA_DB_PASS', None)
