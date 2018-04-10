64cc00750f41a5153aea81a0e3be68825c7cf272
Ivan <ivanyn9@gmail.com>
2018-04-10 09:39:13 +0200

    Añade códigos de municipio a las fuentes
    #18

--- fuentes/actualiza.py
+++ fuentes/actualiza.py
@@ -1,7 +1,11 @@
-from fuentes import Database, fuentes
+from fuentes import Database, Municipios, fuentes
+import pandas as pd
 
 
 def actualiza_fuentes(config):
+    """
+    Actualiza todas las fuentes de datos
+    """
     db = Database.Database(
         database=config.MONGO_DBNAME,
         host=config.MONGO_HOST,
@@ -11,16 +15,41 @@ def actualiza_fuentes(config):
         authSource=config.MONGO_DBNAME
     )
 
+    municipios = carga_fuente(Municipios, db)
+    codigos = municipios.loc[:, ['Municipio', 'Codigo Municipio']]
+
     # Carga los dataframes de cada fuente de datos
     # en una colección con el nombre de la fuente
     for fuente in fuentes:
-        # Instancia la clase y carga el dataframe
-        instancia = fuente()
-        # Carga el dataframe
-        df = instancia.carga()
-        coleccion = instancia.coleccion()
-        # Inserta el dataframe en la base de datos
-        db.carga_datos(df, coleccion)
-        print('{} cargado'.format(coleccion))
+        carga_fuente(fuente, db, codigos)
 
     db.close()
+
+
+def carga_fuente(fuente, db, codigos=None):
+    """
+    Carga una fuente de datos
+    """
+    # Instancia la clase y carga el dataframe
+    instancia = fuente()
+    df = instancia.carga()
+    # Añade el código de municipio
+    if codigos is not None:
+        df = aniade_codigo_municipio(df, codigos)
+    # Inserta el dataframe en la base de datos
+    coleccion = instancia.coleccion()
+    db.carga_datos(df, coleccion)
+    print('{} cargado'.format(coleccion))
+    # Devuelve el dataframe si no se añade el código de municipio
+    if codigos is None:
+        return df
+
+
+def aniade_codigo_municipio(df, codigos):
+    """
+    Añade el código de municipio al dataframe
+    """
+    # Solo si no existe
+    if 'Codigo Municipio' not in df and 'Municipio' in df:
+        df = pd.merge(df, codigos, how='left')
+    return df
