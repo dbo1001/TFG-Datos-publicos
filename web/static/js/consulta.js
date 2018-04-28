5ca8ee7d52283b265355e4b31d02321c63d8db53
Ivan <ivanyn9@gmail.com>
2018-04-28 10:42:12 +0200

    Pestañas para añadir subconsultas
    #25

--- web/static/js/consulta.js
+++ web/static/js/consulta.js
@@ -2,126 +2,191 @@
  * Scripts para actualizar dinámicamente la página de consulta.
  */
 
+var fuentes, columnas, comparadores, valores;
+var mostrar, descripcion, descripcionTitulo;
+
+
+/**
+ * Actualiza las variables globales.
+ * Para elementos generados dinámicamente.
+ */
+function actualizaVariables() {
+    fuentes = $("[name^=fuente-]");
+    columnas = $("[name^=columna_filtro-]");
+    comparadores = $("[name^=comparador-]");
+    valores = $("[name^=valor-]");
+}
+
+/**
+ * Actualiza los eventos
+ */
+function actualizaEventos() {
+    // Elimina eventos existentes
+    fuentes.unbind();
+    columnas.unbind();
+
+    // Actualiza cada vez que cambie el valor de fuente
+    fuentes.on("change", function () {
+        var id = getId(this);
+        actualizaColumnas(id);
+    });
+
+    columnas.on("change", function () {
+        var id = getId(this);
+        actualizaComparador(id);
+    });
+}
+
+function muestraSubconsulta(selector) {
+    var parent = $(selector).parent('li');
+    var activa = parent.hasClass('active');
+    var subconsultas = $(".subconsulta");
+    var tabs = $("#subconsulta-tab li");
+    if (!activa) {
+        var id = tabs.index(parent);
+        subconsultas.hide();
+        var subconsulta = $(subconsultas.get(id));
+        subconsulta.show();
+    }
+}
+
+/**
+ * Obtiene el id de un selector a partir de su nombre
+ */
+function getId(element) {
+    var name = element.getAttribute("name");
+    return name.split("-").pop();
+}
+
+/**
+ * Activa o desactiva el comparador según el parámetro
+ */
+function actualizaComparador(id) {
+    var columna = $(columnas.get(id));
+    var valor = $(valores.get(id));
+    var comparador = $(comparadores.get(id));
+    if (columna.val() === "Todas") {
+        // Desactiva el comparador
+        comparador.attr("disabled", true);
+        valor.attr("disabled", true);
+    } else {
+        // Activa el comparador
+        comparador.removeAttr("disabled");
+        valor.removeAttr("disabled");
+    }
+}
+
+/**
+ * Consulta información sobre una fuente de datos al servidor
+ */
+function datosFuente(nombreFuente, callback) {
+    $.ajax({
+        type: "GET",
+        url: "/api/fuente/" + nombreFuente
+    }).done(callback);
+}
+
+/**
+ * Actualiza la lista de columnas a mostrar
+ */
+function actualizaColumnasMostrar() {
+    var opciones = [];
+    mostrar.empty();
+
+    columnas.each(function(i, selFiltro){
+        var filtro = $(selFiltro);
+        var opcionesColumna = filtro.find("option");
+        opcionesColumna.each(function(j, selColumna) {
+            var columna = $(selColumna);
+            var valor = columna.val();
+            opciones.push(valor);
+        });
+    });
+
+    // Elimina duplicados y columna artificial "Todas"
+    opciones = new Set(opciones);
+    opciones.delete("Todas");
+
+    // Añade las opciones a la lista
+    opciones.forEach(function(valor) {
+        mostrar.append(new Option(valor));
+    });
+}
+
+/**
+ * Actualiza las columnas al seleccionar una fuente
+ */
+function actualizaColumnas(id) {
+    var columna = $(columnas.get(id));
+    var fuente = fuentes.get(id).value;
+
+    // Limpia las columnas
+    columna.attr("disabled", true);
+    columna.empty();
+
+    datosFuente(fuente, function(data) {
+        var columnasFuente = data["columnas"];
+        var descripcionFuente = data["descripcion"];
+
+        columnasFuente.forEach(function(item) {
+            // Añade todas las columnas
+            columna.append(new Option(item));
+        });
+
+        columna.removeAttr("disabled");
+        descripcion.text(descripcionFuente);
+        descripcionTitulo.text(fuente);
+        actualizaComparador(id);
+        actualizaColumnasMostrar();
+    });
+}
+
+/**
+ * Añade una subconsulta
+ */
+function addSubconsulta() {
+    var subconsultas = $(".subconsulta");
+    var ultima = subconsultas.last();
+    var idUltima = subconsultas.index(ultima);
+    var id = idUltima + 1;
+    var nueva = ultima.clone();
+    var html = $(nueva).prop("outerHTML");
+    var regex = new RegExp("-" + idUltima, "g");
+    html = html.replace(regex, "-" + id);
+    $(ultima).after(html);
+}
+
+
+// Al cargar la página
 $(function() {
 
-    /**
-     * Actualiza los campos del formulario según la fuente seleccionada.
-     */
-    var fuentes = $("[name^=fuente-]");
-    var columnas = $("[name^=columna_filtro-]");
-    var mostrar = $("#select_mostrar");
-    var comparadores = $("[name^=comparador-]");
-    var valores = $("[name^=valor-]");
-    var descripcion = $("#descripcion");
-    var descripcionTitulo = $("#descripcion_titulo");
+    mostrar = $("#select_mostrar");
+    descripcion = $("#descripcion");
+    descripcionTitulo = $("#descripcion_titulo");
 
-    /**
-     * Obtiene el id de un selector a partir de su nombre
-     */
-    function getId(element) {
-        var name = element.getAttribute("name");
-        return name.split("-").pop();
-    }
-
-    /**
-     * Activa o desactiva el comparador según el parámetro
-     */
-    function actualizaComparador(id) {
-        var columna = $(columnas.get(id));
-        var valor = $(valores.get(id));
-        var comparador = $(comparadores.get(id));
-        if (columna.val() === "Todas") {
-            // Desactiva el comparador
-            comparador.attr("disabled", true);
-            valor.attr("disabled", true);
-        } else {
-            // Activa el comparador
-            comparador.removeAttr("disabled");
-            valor.removeAttr("disabled");
-        }
-    }
-
-    /**
-     * Consulta información sobre una fuente de datos al servidor
-     */
-    function datosFuente(nombreFuente, callback) {
-        $.ajax({
-            type: "GET",
-            url: "/api/fuente/" + nombreFuente
-        }).done(callback);
-    }
-
-    /**
-     * Actualiza las columnas al seleccionar una fuente
-     */
-    function actualizaColumnas(id) {
-        var columna = $(columnas.get(id));
-        var fuente = fuentes.get(id).value;
-        
-        // Limpia las columnas
-        columna.attr("disabled", true);
-        columna.empty();
-
-        datosFuente(fuente, function(data) {
-            var columnasFuente = data["columnas"];
-            var descripcionFuente = data["descripcion"];
-
-            columnasFuente.forEach(function(item) {
-                // Añade todas las columnas
-                columna.append(new Option(item));
-            });
-
-            columna.removeAttr("disabled");
-            descripcion.text(descripcionFuente);
-            descripcionTitulo.text(fuente);
-            actualizaComparador(id);
-            actualizaColumnasMostrar();
-        });
-    }
-
-    /**
-     * Actualiza la lista de columnas a mostrar
-     */
-    function actualizaColumnasMostrar() {
-        var opciones = [];
-        mostrar.empty();
-
-        columnas.each(function(i, sel_filtro){
-            var filtro = $(sel_filtro);
-            var opciones_columna = filtro.find("option");
-            opciones_columna.each(function(j, sel_columna) {
-                var columna = $(sel_columna);
-                var valor = columna.val();
-                opciones.push(valor);
-            });
-        });
-
-        // Elimina duplicados y columna artificial "Todas"
-        opciones = new Set(opciones);
-        opciones.delete("Todas");
-
-        // Añade las opciones a la lista
-        opciones.forEach(function(valor) {
-            mostrar.append(new Option(valor));
-        });
-    }
+    actualizaVariables();
+    actualizaEventos();
 
     // Llama primero al cargar la página
     columnas.each(function(id){
         actualizaColumnas(id);
     });
 
-    // Actualiza cada vez que cambie el valor de fuente
-    fuentes.on("change", function() {
-        var id = getId(this);
-        actualizaColumnas(id);
+    // Pestañas de la consulta
+    $("#subconsulta-tab").on("click", "li a", function () {
+        muestraSubconsulta(this);
     });
 
-    columnas.on("change", function() {
-        var id = getId(this);
-        console.log(this);
-        console.log(id);
-        actualizaComparador(id);
+    // Evento para añadir una subconsulta
+    $("#add-subconsulta").click(function (e) {
+        e.stopPropagation();
+        var tabs = $("#subconsulta-tab li");
+        var id = tabs.children().length;
+        var item = $("<li><a href=\"#\" data-toggle=\"tab\">Consulta " + id + "</a></li>");
+        addSubconsulta();
+        actualizaVariables();
+        actualizaEventos();
+        $(this).closest('li').before(item);
+        $("#subconsulta-tab li:nth-child(" + id + ") a").click();
     });
-
 });
